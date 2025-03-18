e70ca35b8e042a9ae0d4eba318658d6ce924b101
L3vi Ackerman <l3vi3211@gmail.com>
2025-03-18 15:58:30 +0545

    feat(postgreSQL): establish connect with postgres

--- artistsystem/settings.py
+++ artistsystem/settings.py
@@ -68,13 +68,23 @@ WSGI_APPLICATION = "artistsystem.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "ams",
+        "USER": "super_user",
+        "PASSWORD": "biplav123",
+        "HOST": "localhost",
+        "PORT": "5432",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
