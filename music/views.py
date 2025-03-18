6fbedbffe8ee5ffdc1fdb6a29e3991138494d54b
L3vi Ackerman <l3vi3211@gmail.com>
2025-03-18 10:06:04 +0545

    feat(music): create serializers,view and urls for app/music

--- music/views.py
+++ music/views.py
@@ -1,3 +1,48 @@
 from django.shortcuts import render
 
-# Create your views here.
+from core.models import Music
+from .serializers import MusicSerializer
+from django.http import Http404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+
+
+class MusicList(APIView):
+    def get(self, request, format=None):
+        music = Music.objects.all()
+        serializer = MusicSerializer(music, many=True)
+        return Response(serializer.data)
+
+    def post(self, request, format=None):
+        serializer = MusicSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+
+class MusicDetail(APIView):
+    def get_object(self, pk):
+        try:
+            return Music.objects.get(pk=pk)
+        except Music.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk, format=None):
+        music = self.get_object(pk)
+        serializer = MusicSerializer(music)
+        return Response(serializer.data)
+
+    def put(self, request, pk, format=None):
+        music = self.get_object(pk)
+        serializer = MusicSerializer(music, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        return Response(status=status.HTTP_400_BAD_REQUEST)
+
+    def delete(self, request, pk, format=None):
+        music = self.get_object(pk)
+        music.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
