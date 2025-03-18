182c8353808ea53b3c037e4260b636b6ed714c2f
L3vi Ackerman <l3vi3211@gmail.com>
2025-03-18 09:50:54 +0545

    fix(core): update urls.py

--- artistsystem/urls.py
+++ artistsystem/urls.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", include("users.urls")),
     path("", include("artist.urls")),
     path("", include("profiles.urls")),
+    path("", include(("music.urls"))),
     path("admin/", admin.site.urls),
 ]
