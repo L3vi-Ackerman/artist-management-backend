f3cea58284146d89b3dbccd50cde89043b539d47
L3vi Ackerman <l3vi3211@gmail.com>
2025-04-05 21:38:40 +0545

    fix(profile): fix bug in profile update function

--- profiles/services.py
+++ profiles/services.py
@@ -7,40 +7,40 @@ from django.contrib.auth.hashers import check_password
 def createProfile(userId:int, first_name:str="", last_name="", phone=None, dob=None, address=""):
     with connection.cursor() as cursor:
         cursor.execute(
-            "INSERT INTO core_profile (user_id, first_name,last_name,phone, dob,address, created_at, updated_at) VALUES (%s,%s,%s,%s,%s,%s, %s, %s) RETURNING ID",
-            (
-                userId,
-                first_name,
-                last_name,
-                phone,
-                dob,
-                address,
-                timezone.now(),
-                timezone.now(),
-            ),
-        )
+                "INSERT INTO core_profile (user_id, first_name,last_name,phone, dob,address, created_at, updated_at) VALUES (%s,%s,%s,%s,%s,%s, %s, %s) RETURNING ID",
+                (
+                    userId,
+                    first_name,
+                    last_name,
+                    phone,
+                    dob,
+                    address,
+                    timezone.now(),
+                    timezone.now(),
+                    ),
+                )
         newProfile = cursor.fetchone()
         print(f"newProfile: ", newProfile)
 
     return {
-        "id": newProfile[0],
-        "first_name": first_name,
-        "last_name": last_name,
-        "phone": phone,
-        "dob": dob,
-        "address": address,
-        "created_at":timezone.now(),
-        "updated_at":timezone.now()
-    }
+            "id": newProfile[0],
+            "first_name": first_name,
+            "last_name": last_name,
+            "phone": phone,
+            "dob": dob,
+            "address": address,
+            "created_at":timezone.now(),
+            "updated_at":timezone.now()
+            }
 
 
-def updateProfile(pk, user_id, first_name, last_name, phone, dob, address):
+def updateProfile(first_name, last_name, phone, dob, address, pk:int=None, userId:int=None):
+    if pk:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                    """
 
-    with connection.cursor() as cursor:
-        cursor.execute(
-            """
-            
-            UPDATE core_artist SET 
+            UPDATE core_profile SET 
             first_name = %s, 
             last_name = %s, 
             phone = %s, 
@@ -48,24 +48,42 @@ def updateProfile(pk, user_id, first_name, last_name, phone, dob, address):
             address = %s 
             WHERE id = %s
             RETURNING 
-            id, dob, gender, first_release_year,no_of_albumns_released
-            
-            """,
-            [first_name, last_name, phone, dob, address, pk, user_id],
-        )
+            id, first_name, last_name, phone, dob, address,
 
-        updated_profile = cursor.fetchone()
-        print(updated_profile)
-        if not updated_profile:
-            return None
-        return {
-            "id": updated_profile[0],
-            "first_name": updated_profile[1],
-            "last_name": updated_profile[2],
-            "phone": updated_profile[3],
-            "dob": updated_profile[4],
-            "address": updated_profile[5],
-        }
+            """,
+            [first_name, last_name, phone, dob, address, pk],
+            )
+
+            updated_profile = cursor.fetchone()
+            print(updated_profile)
+            if not updated_profile:
+                return None
+            return {
+                    "id": updated_profile[0],
+                    "first_name": updated_profile[1],
+                    "last_name": updated_profile[2],
+                    "phone": updated_profile[3],
+                    "dob": updated_profile[4],
+                    "address": updated_profile[5],
+                    }
+    else:
+        with connection.cursor() as cursor:
+            cursor.execute(
+                    """
+                     UPDATE core_profile SET 
+            first_name = %s, 
+            last_name = %s, 
+            phone = %s, 
+            dob = %s, 
+            address = %s 
+            WHERE id = %s
+            RETURNING 
+            id, first_name, last_name, phone, dob, address,
+
+            """,
+            [first_name, last_name, phone, dob, address, userId],
+                    
+                    )
 
 
 def deleteProfile(pk):
